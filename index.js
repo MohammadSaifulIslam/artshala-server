75cac3c605ae606ea23ec49c8c56d2e03986f7aa
Mohammad Saiful Islam <saifmdislam231@gmail.com>
2023-06-09 03:21:42 -0700

    user information saved on database route added

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
 const port = process.env.PORT || 5000;
 const app = express();
 
-// midlewire 
+// midlewire
 app.use(cors());
 app.use(express.json());
 
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kgqetuh.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -20,7 +18,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -28,8 +26,54 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
+
+    const usersCollenction = client.db("artshalaDb").collection("users");
+    const classCollenction = client.db("artshalaDb").collection("classes");
+    const instructorCollenction = client
+      .db("artshalaDb")
+      .collection("instructors");
+
+    app.put("/users/:email", async (req, res) => {
+      const user = req.body;
+      const email = req.params.email;
+
+      const query = { email: email };
+      const options = { upsert: true };
+      const updateUser = {
+        $set: {
+          user,
+        },
+      };
+
+      const isExist = await usersCollenction.findOne(query);
+      if (isExist) {
+        return;
+      }
+
+      const result = await usersCollenction.updateOne(
+        user,
+        updateUser,
+        options
+      );
+      res.send(result);
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+    
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -37,14 +81,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("Welcome to Artshala Server");
+});
 
-
-app.get('/', (req, res)=>{
-    res.send("Welcome to Artshala Server")
-})
-
-
-
-app.listen(port, ()=>{
-    console.log(`Artshala server is running on port: ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Artshala server is running on port: ${port}`);
+});
